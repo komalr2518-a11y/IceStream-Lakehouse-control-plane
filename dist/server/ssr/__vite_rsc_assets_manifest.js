cd4a2265c0bd59627253a47d89ff36a7676e7720
komalr2518-a11y <komal.r2518@gmail.com>
2026-09-24 12:01:19 +0530

    Add files via upload

---
+++ dist/server/ssr/__vite_rsc_assets_manifest.js
@@ -0,0 +1,108 @@
+export default {
+  "clientEntryUrl": "/_next/static/chunks/index-B6Vwg7b_.js",
+  "clientEntryDeps": {
+    "js": [
+      "/_next/static/chunks/index-B6Vwg7b_.js",
+      "/_next/static/chunks/rolldown-runtime-hePW80VL.js",
+      "/_next/static/chunks/framework-DTZGTDtF.js",
+      "/_next/static/chunks/vinext-Cz6H68w1.js"
+    ],
+    "css": []
+  },
+  "clientReferenceDeps": {
+    "ececd9e4764b": {
+      "js": [
+        "/_next/static/chunks/ResizeObserverGuard-DwhuzO_h.js",
+        "/_next/static/chunks/rolldown-runtime-hePW80VL.js",
+        "/_next/static/chunks/framework-DTZGTDtF.js",
+        "/_next/static/chunks/index-B6Vwg7b_.js",
+        "/_next/static/chunks/vinext-Cz6H68w1.js"
+      ],
+      "css": []
+    },
+    "6efdf509a785": {
+      "js": [
+        "/_next/static/chunks/page-DP69POTm.js",
+        "/_next/static/chunks/rolldown-runtime-hePW80VL.js",
+        "/_next/static/chunks/framework-DTZGTDtF.js",
+        "/_next/static/chunks/index-B6Vwg7b_.js",
+        "/_next/static/chunks/vinext-Cz6H68w1.js"
+      ],
+      "css": [
+        "/_next/static/css/page.CtuNohLw.css"
+      ]
+    },
+    "9276801271d6": {
+      "js": [
+        "/_next/static/chunks/vinext-Cz6H68w1.js",
+        "/_next/static/chunks/rolldown-runtime-hePW80VL.js",
+        "/_next/static/chunks/framework-DTZGTDtF.js",
+        "/_next/static/chunks/index-B6Vwg7b_.js"
+      ],
+      "css": []
+    },
+    "0b874ad30386": {
+      "js": [
+        "/_next/static/chunks/vinext-Cz6H68w1.js",
+        "/_next/static/chunks/rolldown-runtime-hePW80VL.js",
+        "/_next/static/chunks/framework-DTZGTDtF.js",
+        "/_next/static/chunks/index-B6Vwg7b_.js"
+      ],
+      "css": []
+    },
+    "593f344dc510": {
+      "js": [
+        "/_next/static/chunks/vinext-Cz6H68w1.js",
+        "/_next/static/chunks/rolldown-runtime-hePW80VL.js",
+        "/_next/static/chunks/framework-DTZGTDtF.js",
+        "/_next/static/chunks/index-B6Vwg7b_.js"
+      ],
+      "css": []
+    },
+    "bad85346fc72": {
+      "js": [
+        "/_next/static/chunks/vinext-Cz6H68w1.js",
+        "/_next/static/chunks/rolldown-runtime-hePW80VL.js",
+        "/_next/static/chunks/framework-DTZGTDtF.js",
+        "/_next/static/chunks/index-B6Vwg7b_.js"
+      ],
+      "css": []
+    },
+    "15c18cfaeeff": {
+      "js": [
+        "/_next/static/chunks/layout-segment-context-nHIsOUiN.js",
+        "/_next/static/chunks/rolldown-runtime-hePW80VL.js",
+        "/_next/static/chunks/framework-DTZGTDtF.js",
+        "/_next/static/chunks/vinext-Cz6H68w1.js",
+        "/_next/static/chunks/index-B6Vwg7b_.js"
+      ],
+      "css": []
+    },
+    "8c0f216c4604": {
+      "js": [
+        "/_next/static/chunks/vinext-Cz6H68w1.js",
+        "/_next/static/chunks/rolldown-runtime-hePW80VL.js",
+        "/_next/static/chunks/framework-DTZGTDtF.js",
+        "/_next/static/chunks/index-B6Vwg7b_.js"
+      ],
+      "css": []
+    },
+    "89c3cac48cb5": {
+      "js": [
+        "/_next/static/chunks/vinext-Cz6H68w1.js",
+        "/_next/static/chunks/rolldown-runtime-hePW80VL.js",
+        "/_next/static/chunks/framework-DTZGTDtF.js",
+        "/_next/static/chunks/index-B6Vwg7b_.js"
+      ],
+      "css": []
+    }
+  },
+  "serverResources": {
+    "app/layout.tsx": {
+      "js": [],
+      "css": [
+        "/_next/static/css/index.EnHl0EiU.css"
+      ]
+    }
+  }
+}
